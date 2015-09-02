d306077b2a683d3cd1087ebe180c2c5d4e47a22d
uzair <uzair@Uzairs-MacBook-Pro.local>
2015-09-02 10:09:16 +0400

    Initial Commit

---
+++ PostagainTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PostagainTask
+//
+//  Created by uzair on 02/09/2015.
+//  Copyright (c) 2015 Postagain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PostagainTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PostagainTask
+//
+//  Created by uzair on 02/09/2015.
+//  Copyright (c) 2015 Postagain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
